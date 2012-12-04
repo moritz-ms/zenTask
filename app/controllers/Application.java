08002a83ca13eb3ddb90063ec02423eac221aff6
mario.nolte <mario@nolte-netzwerk.de>
2012-12-04 13:46:36 +0100

    logout functionality with userinformation in main-view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,14 +12,25 @@ public class Application extends Controller {
 
     @Security.Authenticated(Secured.class) //### nicht so richtig klar.
     public static Result index() {
-        return ok(index.render( 
-            Project.find.all(),
-            Task.find.all()
-        )); 
+      return ok(index.render(Project.find.all(), Task.find.all(), User.find.byId(request().username())));
+	//return ok(index.render(
+	//    Project.findInvolving(request().username()),  //limit the projects the user is member of
+	//    Task.find.findTodoInvolving(request().username()), //limit the tasks to the one that the user is assigned to
+	//    User.find.byId(request().username())	//request.username() allows us to access the email adres of the current user 
+	//)); 
     }
+
     public static Result login() {
 	return ok(
 	    login.render(form(Login.class))
+	);
+    }
+
+    public static Result logout() {
+	session().clear(); // clear the session which will logout the user
+	flash("success", "You've been logged out");
+	return redirect(
+	    routes.Application.login()
 	);
     }
 
