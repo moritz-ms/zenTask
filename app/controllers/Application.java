8ca1831ca2a6e357148a50d44574c178e5f74223
mario.nolte <mario@nolte-netzwerk.de>
2012-12-04 10:55:17 +0100

    before rearranging the templates

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,12 @@ import views.html.*;
 
 public class Application extends Controller {
   
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
+
+    public static Result index() {
+        return ok(index.render( 
+            Project.find.all(),
+            Task.find.all()
+        )); 
+    }
   
 }
