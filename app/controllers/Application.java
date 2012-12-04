1975498202b6acf5815801cc04c04eeff4b63f60
mario.nolte <mario@nolte-netzwerk.de>
2012-12-04 12:24:55 +0100

    login functionality

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,4 +16,35 @@ public class Application extends Controller {
             Task.find.all()
         )); 
     }
+    public static Result login() {
+	return ok(
+	    login.render(form(Login.class))
+	);
+    }
+
+    public static Result authenticate() {
+	Form<Login> loginForm = form(Login.class).bindFromRequest();
+	if (loginForm.hasErrors()) { //if validation fails we return a status of 400 Bad Request
+	    return badRequest(login.render(loginForm));
+	} else { // if validation was successful we put an attribute email with its values to the session
+	    session().clear();
+	    session("email", loginForm.get().email);
+	    return redirect(
+		routes.Application.index() // after setting the user in the session we issue an HTTP redirect to the dashboard.
+	    );
+	}
+    }
+    
+    public static class Login {
+	    public String email;
+	    public String password;
+	  
+	    public String validate() {
+		    if(User.authenticate(email, password) == null) {
+			    return "Invalid user or password";
+		    }
+		    return null;
+		  
+	    }
+    }
 }
